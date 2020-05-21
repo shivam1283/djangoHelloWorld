9b44d2034438f46908449fd82721152429f71f18
Shivam Yadav <shivam@getvital.in>
2020-05-22 01:11:07 +0530

    [FEATURE] Added profile feed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from profiles_project import settings
 
 class UserProfileManager(BaseUserManager):
 	"""Manager for user Profiles"""
@@ -47,3 +47,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 	def __str__(self):
 		"""Return string representation of our user"""
 		return self.email
+
+
+class ProfileFeedItem(models.Model):
+	"""Profile status update"""
+	user_profile = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE
+	)
+	status_text = models.CharField(max_length=255)
+	created_on = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.status_text
--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,13 @@ class UpdateOwnProfile(permissions.BasePermission):
 			return True
 
 		return request.user.id == obj.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+	"""Allow user to update their own status"""
+
+	def has_object_permission(self, request, view, obj):
+		"""Check the user is trying to update their own status"""
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		return request.user.id == obj.user_profile.id
